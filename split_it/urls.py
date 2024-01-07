9ebd906c824afca07364274e67e0bc52cbdcc468
GoKU <koushikmallik001@gmail.com>
2024-01-08 00:57:44 +0530

    desgining.

--- split_it/urls.py
+++ split_it/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,5 +8,7 @@ urlpatterns = [
     path('', include('users.urls')),
     path('', include('expenses.urls')),
     path('', include('dashboard.urls')),
-    path('home/', include('home.urls')),  # Add a new app for the homepage
+    path('', include('home.urls')),  # Add a new app for the homepage
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
