8ef309e0c4e82dca26794239d31aadb3cb629464
GoKU <koushikmallik001@gmail.com>
2024-01-08 09:32:17 +0530

    desgining.

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, template_name='home/homepage.html', context={"is_logged_in": True})
+    return render(request, template_name='home/homepage.html', context={"is_logged_in": False})
