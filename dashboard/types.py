03d97bfc74b1922f8b8400023d4df3448912bc0b
GoKU <koushikmallik001@gmail.com>
2024-01-16 01:10:59 +0530

    Added expense balance calculation

--- dashboard/types.py
+++ dashboard/types.py
@@ -1,16 +1,24 @@
 import json
 import uuid
-from typing import Optional
+from typing import Optional, List
 
+from decimal import Decimal
 from pydantic import BaseModel
+
+from expenses.utils import ExpenseDetails
 
 
 class ExportGroupDetails(BaseModel):
     id: Optional[uuid.UUID] = None
     name: Optional[str] = None
     members: Optional[list] = None
+    expenses: Optional[List[ExpenseDetails]] = None
+    total_balance: Optional[Decimal] = None
+    you_paid: Optional[Decimal] = None
+    your_balance: Optional[Decimal] = None
 
     def __init__(self, **kwargs):
         if kwargs.get("members"):
             kwargs["members"] = json.loads(kwargs.get("members"))
+
         super().__init__(**kwargs)
--- dashboard/utils.py
+++ dashboard/utils.py
@@ -1,11 +1,32 @@
+from decimal import Decimal
+
 from dashboard.models import ExpenseGroup
 from dashboard.types import ExportGroupDetails
+from expenses.models import Expense
+from expenses.utils import ExpenseDetails
 
 
 class GroupUtils:
-    def get_group_details(self, group_id):
+    @staticmethod
+    def get_group_details(request, group_id):
         if group_id:
             if ExpenseGroup.objects.filter(id=group_id).exists():
                 group = ExpenseGroup.objects.get(id=group_id)
+                expenses = Expense.objects.filter(group__id=group_id)
+                export_expenses = []
+                total_balance = Decimal(0.0)
+                you_paid = Decimal(0.0)
+                your_balance = Decimal(0.0)
+                for expense in expenses:
+                    temp_expense = ExpenseDetails(**expense.model_to_dict())
+                    _, temp_you_paid = temp_expense.calculate_your_balance(request)
+                    export_expenses.append(temp_expense)
+                    total_balance += temp_expense.amount_paid
+                    your_balance += temp_expense.your_balance
+                    you_paid += temp_you_paid
                 group = ExportGroupDetails(**group.model_to_dict())
+                group.expenses = export_expenses
+                group.your_balance = your_balance
+                group.you_paid = you_paid
+                group.total_balance = total_balance
                 return group
